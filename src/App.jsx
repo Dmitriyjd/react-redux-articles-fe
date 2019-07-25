c762e86b9ea8625ecc7f6b4bff753aa0596e4c48
dmitriyjd <dimon05091996abcd@mail.ru>
2019-07-25 10:56:15 +0300

    Add eslint config

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'
 import Home from './pages/Home'
 
-function App() {
+function App () {
   return (
     <div className="App">
-      <Home/>
+      <Home />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
