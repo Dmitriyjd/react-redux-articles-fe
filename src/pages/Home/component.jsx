46973752749536fdcbee1cdc4dff0c5b84afff11
dmitriyjd <dimon05091996abcd@mail.ru>
2019-07-25 11:42:10 +0300

    Create <TableActionButtons/> block Fix eslint errors

--- src/pages/Home/component.jsx
+++ src/pages/Home/component.jsx
@@ -1,34 +1,30 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import { columns, mockDataSource } from './tableConfig';
-import { Table } from 'antd';
-import { dataSource } from './tableConfig';
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { columns } from './tableConfig'
+import { Table } from 'antd'
 
 import { HomePageWrapper } from './styles'
 
 class Home extends PureComponent {
-    render() {
-        return(
-            <HomePageWrapper>
-                <Table
-                    columns={ columns }
-                    dataSource={ mockDataSource }
-                />
-            </HomePageWrapper>
-        )
-    }
+  render () {
+    return (
+      <HomePageWrapper>
+        <Table
+          columns={columns}
+          dataSource={this.props.articles} />
+      </HomePageWrapper>
+    )
+  }
 }
 
 Home.propTypes = {
-    getArticles: PropTypes.func.isRequired,
-    updateArticle: PropTypes.func.isRequired,
-    articles: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.string,
-            title: PropTypes.string,
-            Body: PropTypes.string
-        }),
-    ).isRequired,
+  articles: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      title: PropTypes.string,
+      Body: PropTypes.string,
+    }),
+  ).isRequired,
 }
 
-export default Home;
+export default Home
